829eaa7a7f321bbf5b32e3cca7e400beb26bd888
sh-pers <sandra_huett@persistent.com>
2021-06-15 09:29:40 +0200

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -202,8 +202,7 @@ exports.validate = function (req, res) {
 
     const mcAuthHttps = require('https')
 
-    const payload = '{"grant_type": "client_credentials","client_id": "5t02s8dmqrx39d98sbuvy8e8","client_secret": ';
-    payload += '"tDkBpuJkty7JDiQSZyWhCumi", "scope": "data_extensions_read data_extensions_write"}';
+    const payload = '{"grant_type": "client_credentials","client_id": "5t02s8dmqrx39d98sbuvy8e8","client_secret": "tDkBpuJkty7JDiQSZyWhCumi", "scope": "data_extensions_read data_extensions_write"}';
     console.log('auth payload: ', payload);
     const mcAuthData = payload; //JSON.stringify(payload);
 
