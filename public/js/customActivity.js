066c6342308eb09e24590d5cdf665ac65107104f
sh-pers <sandra_huett@persistent.com>
2021-06-16 11:02:03 +0200

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -125,11 +125,51 @@ define([
         var contentJSON = getcontentJSON();
         var preObject;
         var firstName;
+        var lastName;
+        var accountId;
+        var opportunityId;
+        var stageName;
+        var programFamily;
+        var cpAccountId;
+        var salutation;
+        var productName;
+        var productFamily;
+        var productId;
+        var degree;
+        var createdDate;
+        var optIn;
+        var country;
+        var voucher;
+        var workExperience;
+        var budget;
+        var rate;
+        var studyAdvisor;
+
+
 
         switch(entryObject)
         {
         case 'Opportunity':
-          firstName = 'Opportunity:Account:FirstName';
+            firstName = 'Opportunity:Account:FirstName';
+            lastName = 'Opportunity:Account:LastName';
+            accountId = 'Opportunity:Account:Id';
+            opportunityId = 'Opportunity:Id';
+            stageName = 'Opportunity:StageName';
+            programFamily = 'Opportunity:ProgramFamily__c';
+            cpAccountId = 'Opportunity:Account:CP_Account_ID__c';
+            salutation = 'Opportunity:Account:Salutation';
+            productName = 'Opportunity:CourseOfStudyLookup__r:Product_Display_Name__c';
+            productFamily = 'Opportunity:CourseOfStudyLookup__r:Family';
+            productId = 'Opportunity:CourseOfStudyLookup__r:Id';
+            degree = 'Opportunity:DegreeName__c';
+            createdDate = 'Opportunity:CreatedDate';
+            optIn = 'Opportunity:Account:OptInEmail__c';
+            country = 'Opportunity:Account:ShippingCountry';
+            voucher = 'Opportunity:VoucherID__c';
+            workExperience = 'Opportunity:WorkExperience__c';
+            budget = 'Opportunity:BudgetPerMonth__c';
+            rate = 'Opportunity:RatePerInterval__c';
+            studyAdvisor = 'Opportunity:StudyAdvisor__c';
           break;
         case 'OpportunityArchive__c':
           firstName = 'OpportunityArchive__c:Account__r:FirstName'
@@ -140,24 +180,16 @@ define([
         payload.name = name;
 
         payload['arguments'].execute.inArguments = [
-            {
-                "tokens": authTokens 
-            },
-            { 
-                "url": url 
-            }, 
-            { 
-                "contentJSON": contentJSON
-            },
-            {
-                "emailAddress": "{{InteractionDefaults.Email}}"
-            },
-            {
-                "EntryObject": entryObject,
-            },
-            {
-                "firstName": "{{Event."+ eventDefinitionKey + ".\"" + firstName + "\"}}",
-            }
+             "tokens": authTokens,
+             "EntryObject": entryObject,
+             "url": url, 
+             "contentJSON": contentJSON,
+             "contactId": "{{Contact.Key}}",
+             "email": "{{InteractionDefaults.Email}}",
+             "phone": "{{InteractionDefaults.Phone}}",
+             "firstName": "{{Event."+ eventDefinitionKey + ".\"" + firstName + "\"}}",
+             "firstName1": "{{Event."+ eventDefinitionKey + ".\"" + firstName + "\"}}",
+             "firstName2": "{{Event."+ eventDefinitionKey + ".\"" + firstName + "\"}}"
         ];
         
         payload['metaData'].isConfigured = true;
