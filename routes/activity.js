d0acaff724af701749a763922b9f2c24b5ac0d5e
sh-pers <sandra_huett@persistent.com>
2021-06-22 11:08:08 +0200

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -158,7 +158,7 @@ exports.execute = function (req, res) {
             const zapHttps = require('https')
 
             var zapData = JSON.stringify(contentJSON)
-            zapData = zapData.replace(/\n/g, "");
+            zapData = zapData.replace(/\\n/g, "");
             console.log('Replaced Payload: ', zapData);
 
             var zapOptions = {
